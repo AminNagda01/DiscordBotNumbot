600de82d4f1242aafa830d257783f86015e9314a
Muhammad Amin <aminworkm@gmail.com>
2023-10-03 01:36:32 -0400

    Bot works with generic implementation

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
-// Require the necessary discord.js classes
-const Discord = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-// Create a new client instance
-const client = new Discord.Client({
-        intents: [
-        "GUILDS",
-        "GUILD_MESSAGES"
-      ]
-    });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-client.once("ready", () => {
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		}
+    else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, () => {
 	console.log('Ready!');
 });
 
-client.on("messageCreate", (message) => {
-    const args = message.content;
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-    if(args.startsWith("!")) {
-      switch (args) {
-        case "!bruh" :
-          message.reply("19 dollar fortnite card");
-          break;
-        case "!u smell" :
-          message.reply("no u");
-          break;
-        default :
-          break;
-      }
-    }
-    // else {
-    //   const gifSetter = args.split(" ");
-    //   const linktest = gifSetter.toString();
-    //   if(gifSetter.length == 1 && linktest.substring(0, 6) != "https:") {
-    //     message.reply("https://media.tenor.com/DXeA2HkpYcsAAAAC/anime-kurisu.gif")
-    //   }
-    // }
+	const command = client.commands.get(interaction.commandName);
 
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	}
+  catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+    else {
+			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		}
+	}
 });
 
-// Login to Discord with your client's token
 client.login(token);
